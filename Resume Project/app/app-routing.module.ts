c95d66c3871993a59d4f6e67e82be2be1e000847
Normalito Enriquez III <84098445+nmenriquez@users.noreply.github.com>
2022-02-21 22:06:38 +0800

    Add files via upload

---
+++ Resume Project/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+
+const routes: Routes = [{path:'about', component:AboutComponent},
+{path:'skills', component:SkillsComponent},
+{path:'portfolio', component:PortfolioComponent},
+{path:'contact', component:ContactComponent}];
+
+@NgModule({
+  declarations:[],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
