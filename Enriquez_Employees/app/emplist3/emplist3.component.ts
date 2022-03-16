493d561180cc45fc359816181efaf959f24e20da
Normalito Enriquez III <84098445+nmenriquez@users.noreply.github.com>
2022-03-16 12:59:32 +0800

    Add files via upload

---
+++ Enriquez_Employees/app/emplist3/emplist3.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { MyserviceService } from '../myservice.service';
+
+@Component({
+  selector: 'app-emplist3',
+  templateUrl: './emplist3.component.html',
+  styleUrls: ['./emplist3.component.css']
+})
+export class Emplist3Component implements OnInit {
+  EmployeeInfo: any;
+  constructor(private myservice:MyserviceService) { }
+
+  ngOnInit(): void {
+    this.EmployeeInfo = this.myservice.allNames();
+  }
+
+}
---
+++ Enriquez_Employees/app/myservice.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MyserviceService {
+
+  constructor() { }
+
+  allNames(){
+    return[
+      {
+      id:101,
+      firstname:'Joseph',
+      lastname:'Dizon',
+      email:'jdizon@hau.edu.ph'
+      },
+      {
+        id:102,
+        firstname:'James',
+        lastname:'Atienza',
+        email:'jatienza@hau.edu.ph'
+        },
+        {
+          id:103,
+          firstname:'John',
+          lastname:'Cena',
+          email:'jcena@hau.edu.ph'
+          },
+          {
+            id:104,
+            firstname:'Robert',
+            lastname:'Quintana',
+            email:'rquintana@hau.edu.ph'
+            },
+            {
+              id:105,
+              firstname:'Normalito',
+              lastname:'Enriquez',
+              email:'nenriquez@hau.edu.ph'
+              }
+    ]
+  }
+}
